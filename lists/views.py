396ff3214308b2beabc7acdfaaf75d67957e2001
neilmcb <neilmcblane@googlemail.com>
2020-11-24 17:21:16 +0000

    Add POST form to site

--- lists/views.py
+++ lists/views.py
@@ -1,5 +1,10 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 
 def homepage(request):
-	return render(request, 'homepage.html')
+	return render(
+		request,
+		'homepage.html',
+		{'new_item_text': request.POST.get('item_text', '')},
+	)
