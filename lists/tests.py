66c9247a4dc5bc37cb5e3a932ca3f92b7b6c8035
neilmcb <neilmcblane@googlemail.com>
2020-11-24 17:37:13 +0000

    Add model for list items and associated migration

--- lists/tests.py
+++ lists/tests.py
@@ -3,7 +3,7 @@ from django.test import TestCase
 from django.urls import resolve
 
 from .views import homepage
-
+from .models import Item
 
 class HomepageTest(TestCase):
 
@@ -16,3 +16,24 @@ class HomepageTest(TestCase):
 		self.assertIn('A new list item', response.content.decode())
 		self.assertTemplateUsed(response, 'homepage.html')
 
+
+
+class ItemModelTest(TestCase):
+	
+	def test_saving_and_retrieving_items(self):
+		first_item = Item()
+		first_item.text = 'The first (ever) list item'
+		first_item.save()
+
+		second_item = Item()
+		second_item.text = 'Item the second'
+		second_item.save()
+
+		saved_items = Item.objects.all()
+		self.assertEqual(saved_items.count(), 2)
+
+		first_saved_item = saved_items[0]
+		self.assertEqual(first_saved_item.text, 'The first (ever) list item')
+		second_saved_item = saved_items[1]
+		self.assertEqual(second_saved_item.text, 'Item the second')
+		
