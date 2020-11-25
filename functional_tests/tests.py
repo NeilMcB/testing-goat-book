2f248f9329ef353fa99351fc82e98a09f41953da
neilmcb <neilmcblane@googlemail.com>
2020-11-25 15:38:13 +0000

    Use LiveServerTestCase to deal with tidying up test database in functional tests

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,11 +1,12 @@
 import time
 import unittest
 
+from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
 
-class NewVisitorTest(unittest.TestCase):
+class NewVisitorTest(LiveServerTestCase):
 
 	def setUp(self):
 		self.browser = webdriver.Firefox()
@@ -23,7 +24,7 @@ class NewVisitorTest(unittest.TestCase):
 
 	def test_can_start_a_list_and_retrive_it_later(self):
 		# Gussie has heard about a cool new app, she goes to look at the homepage.
-		self.browser.get('http://localhost:8000')
+		self.browser.get(self.live_server_url)
 
 		# She notices the page title and header mention to-do lists.
 		self.assertIn('To-Do', self.browser.title)
@@ -70,8 +71,4 @@ class NewVisitorTest(unittest.TestCase):
 		self.fail('Finish the test!')
 		
 		# She visits that URL - her to-do list is still there.
-
-
-if __name__ == '__main__':
-	unittest.main()
 
