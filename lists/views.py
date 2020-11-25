b75ada0e8522eb1499e5c345b59136336164eaa1
neilmcb <neilmcblane@googlemail.com>
2020-11-25 20:18:00 +0000

    More shit

--- lists/views.py
+++ lists/views.py
@@ -4,12 +4,14 @@ from .models import Item
 
 
 def homepage(request):
-	if request.method == 'POST':
-		Item.objects.create(text=request.POST['item_text'])
-		return redirect('/lists/the-only-list-in-the-world/')
-
 	return render(request, 'homepage.html')
 
 
 def view_list(request):
 	return render(request, 'list.html', {'items': Item.objects.all()})
+
+
+def new_list(request):	
+	Item.objects.create(text=request.POST['item_text'])
+	return redirect('/lists/the-only-list-in-the-world/')
+
--- superlists/urls.py
+++ superlists/urls.py
@@ -20,5 +20,6 @@ from lists import views
 
 urlpatterns = [
 	url(r'^$', views.homepage, name='homepage'),
+	url(r'^lists/new$', views.new_list, name='new_list'),
 	url(r'^lists/the-only-list-in-the-world/$', views.view_list, name='view_list'),
 ]
