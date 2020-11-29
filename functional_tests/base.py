98a43608f4bcbc80d30a087f02a8b84a2de0bc03
neilmcb <neilmcblane@googlemail.com>
2020-11-29 18:55:18 +0000

    Add full empty list validation test

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -18,6 +18,17 @@ class FunctionalTest(StaticLiveServerTestCase):
 	def tearDown(self):
 		self.browser.quit()
 
+	def wait_for(self, fn):
+		start_time = time.time()
+		while True:
+			try:
+				return fn()
+			except (AssertionError, WebDriverException) as e:
+				if time.time() - start_time > MAX_WAIT:
+					raise e
+				else:
+					time.sleep(0.5)
+	
 	
 	def check_for_row_in_list_table(self, row_text):
 		table = self.browser.find_element_by_id('id_list_table')
