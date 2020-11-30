436f89555d2bf2375f08b4a888c8cb26c8bd66ef
neilmcb <neilmcblane@googlemail.com>
2020-11-30 11:57:39 +0000

    Use django shortcuts to redirect to model pages

--- lists/tests/test_models.py
+++ lists/tests/test_models.py
@@ -42,3 +42,8 @@ class ListAndItemModelTest(TestCase):
 			item.save()
 			# Force database to perform validation
 			item.full_clean()
+
+	def test_get_absolute_url(self):
+		list_ = List.objects.create()
+		self.assertEqual(list_.get_absolute_url(), f'/lists/{list_.id}/')
+
