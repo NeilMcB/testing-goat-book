6663408f49368849b65409f1bc38b45805166aee
neilmcb <neilmcblane@googlemail.com>
2020-12-02 18:20:53 +0000

    Add model-level validation to prevent duplicate item entry (FT failing)

--- lists/tests/test_models.py
+++ lists/tests/test_models.py
@@ -6,34 +6,17 @@ from django.urls import resolve
 from lists.views import homepage
 from lists.models import Item, List
 
-class ListAndItemModelTest(TestCase):
-	
-	def test_saving_and_retrieving_items(self):
-		list_ = List()
-		list_.save()	
+class ItemModelTest(TestCase):
 
-		first_item = Item()
-		first_item.text = 'The first (ever) list item'
-		first_item.list = list_	
-		first_item.save()
+	def test_default_text(self):
+		item = Item()
+		self.assertEqual(item.text, '')
 
-		second_item = Item()
-		second_item.text = 'Item the second'
-		second_item.list = list_	
-		second_item.save()
-
-		saved_list = List.objects.first()
-		self.assertEqual(saved_list, list_)
-
-		saved_items = Item.objects.all()
-		self.assertEqual(saved_items.count(), 2)
-
-		first_saved_item = saved_items[0]
-		self.assertEqual(first_saved_item.text, 'The first (ever) list item')
-		self.assertEqual(first_saved_item.list, list_)
-		second_saved_item = saved_items[1]
-		self.assertEqual(second_saved_item.text, 'Item the second')
-		self.assertEqual(second_saved_item.list, list_)
+	def test_item_is_related_to_list(self):
+		list_ = List.objects.create()
+		item = Item(list=list_)
+		item.save()
+		self.assertIn(item, list_.item_set.all())	
 
 	def test_cannot_save_empty_list_items(self):
 		list_ = List.objects.create()
@@ -43,7 +26,39 @@ class ListAndItemModelTest(TestCase):
 			# Force database to perform validation
 			item.full_clean()
 
+	def test_duplicate_items_in_same_list_are_invalid(self):
+		list_ = List.objects.create()
+		Item.objects.create(text='foo', list=list_)
+		with self.assertRaises(ValidationError):
+			item = Item(text='foo', list=list_)
+			item.full_clean()
+
+	def test_duplicate_items_in_different_lists_are_valid(self):
+		list1 = List.objects.create()
+		list2 = List.objects.create()
+		
+		Item.objects.create(text='foo', list=list1)
+		item = Item(text='foo', list=list2)
+		item.full_clean()  # Shouldn't raise an error
+
+	def test_list_ordering(self):
+		# Uniqueness constraint in Item can cause issues with query ordering
+		list_ = List.objects.create()
+		item1 = Item.objects.create(text='1', list=list_)
+		item2 = Item.objects.create(text='2', list=list_)
+		item3 = Item.objects.create(text='3', list=list_)
+		self.assertEqual(
+			list(Item.objects.all()),
+			[item1, item2, item3],
+		)
+	
+	def test_string_representation(self):
+		item = Item(text='some text')
+		self.assertEqual(str(item), 'some text')
+
+
+class ListModelTestCase(TestCase):
+	
 	def test_get_absolute_url(self):
 		list_ = List.objects.create()
 		self.assertEqual(list_.get_absolute_url(), f'/lists/{list_.id}/')
-
