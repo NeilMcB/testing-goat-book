3f70370b22950d939e7c70a4f12952b652734b37
neilmcb <neilmcblane@googlemail.com>
2020-12-03 18:22:38 +0000

    Clear error messages on click too

--- lists/static/list.js
+++ lists/static/list.js
@@ -2,5 +2,9 @@ window.Superlists = {}
 window.Superlists.initialize = () => {
 	$('input[name="text"]').on('keypress', () => {
 		$('.has-error').hide();
+	});
+
+	$('input[name="text"]').on('click', () => {
+		$('.has-error').hide();
 	});	
 };
