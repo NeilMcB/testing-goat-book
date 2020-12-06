2246b081a3c623668fa3e58a185f4c32d73934cc
neilmcb <neilmcblane@googlemail.com>
2020-12-06 18:50:50 +0000

    Add token generation and login email link@

--- accounts/views.py
+++ accounts/views.py
@@ -1,13 +1,30 @@
+from django.contrib import messages
 from django.core.mail import send_mail
+from django.core.urlresolvers import reverse
 from django.shortcuts import redirect, render
 
+from .models import Token
 
 def send_login_email(request):
 	email = request.POST['email']
+	token = Token.objects.create(email=email)
+
+	url = request.build_absolute_uri(
+		f'{reverse("login")}?token={str(token.uid)}'
+	)
+	message_body = f'Use this link to log in:\n\n{url}'
+
 	send_mail(
 		'Your login link for Superlists',
-		'body',
+		message_body,
 		'noreply@superlists.com',
 		[email]
 	)
+	messages.success(
+		request,
+		'Check your email, we\'ve sent you a link you can use to log in.',
+	)
+	return redirect('/')
+
+def login(request):
 	return redirect('/')
