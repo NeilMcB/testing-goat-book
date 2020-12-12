40a99dbcfdc2e9f155908ade486dbafd228e7608
neilmcb <neilmcblane@googlemail.com>
2020-12-12 15:30:55 +0000

    Convert to f-string

--- functional_tests/server_tools.py
+++ functional_tests/server_tools.py
@@ -8,7 +8,7 @@ def _get_manage_dot_py(host):
 
 
 def _get_server_env_vars(host):
-	env_lines = run('cat ~/sites/{host}/.env').splitlines()
+	env_lines = run(f'cat ~/sites/{host}/.env').splitlines()
 	return dict(l.split('=') for l in env_lines if l)
 
 
